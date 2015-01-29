e113f72f772bc0be2ba7d6c5f7194b33322b7393
Karthik Ram <karthik.ram@gmail.com>
2015-01-29 11:37:30 -0800

    Added roxygen tags inline #2

--- R/findHost.R
+++ R/findHost.R
@@ -1,30 +1,55 @@
-findHost <-function(genus, species='', location=''){
-
-possLocs=listLocations()
-if(location %in% possLocs == FALSE & location != ''){stop("Please choose a location from the possible locations in the listLocations() function")}
-
-if(location != ''){
-location1=gsub("\\+", "%2B", location)
-location2=gsub("\\(", "%28", location1)
-location3=gsub("\\)", "%29", location2)
-location4=gsub(" ", "+", location3)
-location=location4
-}
-
-hpUrl=html(paste("http://www.nhm.ac.uk/research-curation/scientific-resources/taxonomy-systematics/host-parasites/database/results.jsp?dbfnsRowsPerPage=50000&x=13&y=5&paragroup=&fmsubgroup=Starts+with&subgroup=&fmparagenus=Starts+with&paragenus=&fmparaspecies=Starts+with&paraspecies=&fmhostgenus=Contains&hostgenus=",genus,"&fmhostspecies=Contains&hostspecies=", species,"&location=",location,"&hstate=&pstatus=&showparasites=on&showhosts=on&showrefs=on&groupby=parasite&search=Search", sep=''))
-
-	names <- hpUrl %>% 
-		html_nodes(".searchlink")%>%
-		html_text()
-
-	hpList = matrix(names, ncol=2, byrow=TRUE)
-	parNames=sapply(hpList[,1], strsplit, ' ')
-	parNames2=lapply(parNames, function(a){if(length(a)<2){return(a)}else{return(paste(a[1], a[2], sep=' '))}})
-	parNames3=lapply(parNames2, function(a){ gsub("\\(|\\)", "", a)})
-
-	names(parNames3)=NULL
-	parNamesShort=unlist(parNames3)
-	ret=data.frame(Host=hpList[,2], Parasite=parNamesShort, ParasiteFull=hpList[,1])
-return(ret)
-
-}
+#' Find parasite occurrence data for given host.
+#' 
+#' Given a host genus, species, and/or location, returns a list of parasite
+#' occurrences on that host or for that location. Search available locations
+#' using the `listLocations` function.
+#' 
+#' `location` values can be listed using the `listLocations()` function.
+#' 
+#' @param genus Host genus
+#' @param species Host species
+#' @param location Geographic location.
+#' @return Three column matrix containing host species, and parasite species
+#' (shortened name and full name), with each row corresponding to an occurrence
+#' of a parasite species on a host species.
+#' @author Tad Dallas
+#' @seealso \code{\link{findParasite}}
+#' @references Gibson, D. I., Bray, R. A., & Harris, E. A. (Compilers) (2005).
+#' Host-Parasite Database of the Natural History Museum, London. URL.
+#' @examples
+#' 
+#' gorillaParasites=findHost('Gorilla', 'gorilla')
+#' head(gorillaParasites)
+#' 
+findHost <- function(genus, species = "", location = "") {
+    possLocs <- listLocations()
+    if (location %in% possLocs == FALSE & location != "") {
+        stop("Please choose a location from the possible locations in the listLocations() function")
+    }
+    if (location != "") {
+        location1 <- gsub("\\+", "%2B", location)
+        location2 <- gsub("\\(", "%28", location1)
+        location3 <- gsub("\\)", "%29", location2)
+        location4 <- gsub(" ", "+", location3)
+        location <- location4
+    }
+    hpUrl <- html(paste("http://www.nhm.ac.uk/research-curation/scientific-resources/taxonomy-systematics/host-parasites/database/results.jsp?dbfnsRowsPerPage=50000&x=13&y=5&paragroup=&fmsubgroup=Starts+with&subgroup=&fmparagenus=Starts+with&paragenus=&fmparaspecies=Starts+with&paraspecies=&fmhostgenus=Contains&hostgenus=", 
+        genus, "&fmhostspecies=Contains&hostspecies=", species, "&location=", location, "&hstate=&pstatus=&showparasites=on&showhosts=on&showrefs=on&groupby=parasite&search=Search", sep = ""))
+    names <- hpUrl %>% html_nodes(".searchlink") %>% html_text()
+    hpList <- matrix(names, ncol = 2, byrow = TRUE)
+    parNames <- sapply(hpList[, 1], strsplit, " ")
+    parNames2 <- lapply(parNames, function(a) {
+        if (length(a) < 2) {
+            return(a)
+        } else {
+            return(paste(a[1], a[2], sep = " "))
+        }
+    })
+    parNames3 <- lapply(parNames2, function(a) {
+        gsub("\\(|\\)", "", a)
+    })
+    names(parNames3) <- NULL
+    parNamesShort <- unlist(parNames3)
+    ret <- data.frame(Host = hpList[, 2], Parasite = parNamesShort, ParasiteFull = hpList[, 1])
+    return(ret)
+} 
--- R/listLocations.R
+++ R/listLocations.R
@@ -1,7 +1,26 @@
+#' Lists geographic locations
+#' 
+#' Lists geographic locations that can be input to `findHost()` or
+#' `findParasite()`
+#' 
+#' 
+#' @return Returns a vector of locations. However, it is important to note that
+#' some locations are nested inside of other locations. For instance, you can
+#' search for all host-parasite associations in the Great Lakes as a whole, or
+#' you can target Lake Ontario specifically. Currently, I don't have a good way
+#' of showing nested structure of locations.
+#' @author Tad Dallas
+#' @references Gibson, D. I., Bray, R. A., & Harris, E. A. (Compilers) (2005).
+#' Host-Parasite Database of the Natural History Museum, London. URL.
+#' @examples
+#' 
+#' listLocations()
+#' 
+#' 
 listLocations <-
 function(){
 
-locationUrl=html(paste("http://www.nhm.ac.uk/research-curation/scientific-resources/taxonomy-systematics/host-parasites/database/location.jsp?location=&paragroup=&showparasites=on&paraspecies=&fmhostgenus=Contains&fmparagenus=Starts+with&showrefs=on&fmsubgroup=Starts+with&groupby=parasite&pstatus=&showhosts=on&hostspecies=&hostgenus=&paragenus=&fmparaspecies=Starts+with&subgroup=&fmhostspecies=Contains&hstate=&getlocation=select"))
+locationUrl <- html(paste("http://www.nhm.ac.uk/research-curation/scientific-resources/taxonomy-systematics/host-parasites/database/location.jsp?location=&paragroup=&showparasites=on&paraspecies=&fmhostgenus=Contains&fmparagenus=Starts+with&showrefs=on&fmsubgroup=Starts+with&groupby=parasite&pstatus=&showhosts=on&hostspecies=&hostgenus=&paragenus=&fmparaspecies=Starts+with&subgroup=&fmhostspecies=Contains&hstate=&getlocation=select"))
 
 locations <- locationUrl %>% 
 html_nodes("option") %>%
