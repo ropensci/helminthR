043075fbff120a9ffb0aebeec2018562e4bb397e
arw36 <arwill19@gmail.com>
2020-01-04 14:16:17 -0500

    add function to query citations

---
+++ R/findCitations.R
@@ -0,0 +1,34 @@
+#' Find citations for given host, parasite, or location query.
+#'
+#' Given a previous query of host, parasite, or location host-parasite occurences, returns a list of citations.
+#'
+#' @param interactions Interaction dataframe from previous query (findLocation, findParasite, or findHost)
+
+#'
+#' @return One column data.frame containing primary literature citation as provided by LMNH
+#'
+#' @author Anna Willoughby
+
+library(xml2)
+library(purrr)
+library(rvest)
+
+# input needs to be data.frame with Reference urls in column 4
+findCitations <- function(interactions = NULL){
+  ref_df  <- NULL;
+  for(i in 1:nrow(interactions)){
+    url <- interactions[i,4] # first reference link, will need to loop for all
+    webpage <- read_html(url) #pull text from webpage
+    cdtble <- html_node(webpage, "table") # extract reference table from webpage
+    cdf <- html_table(cdtble, fill = TRUE) # convert to dataframe
+    cdf <- cdf[,c(2,3)] # only keep columns of interest
+    names(cdf) <- c("variable", "citations") # name columns
+    cdf <- filter(cdf, variable == "Reference:") # filter to just the reference
+    cdf$variable <- NULL # remove column
+    ref_df <- rbind(ref_df, cdf) # add to final dataframes
+    }
+  ref_df %<>% unique() # remove duplicates
+  return(ref_df)
+}
+
+# currently this limits to 30 references per interaction
