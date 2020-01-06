66c2510863df25d907459fe9989f84ab82c08a81
arw36 <arwill19@gmail.com>
2020-01-06 17:29:32 -0500

    add original as parameter

--- R/findCitations.R
+++ R/findCitations.R
@@ -3,31 +3,61 @@
 #' Given a previous query of host, parasite, or location host-parasite occurences, returns a list of citations.
 #'
 #' @param interactions Interaction dataframe from previous query (findLocation, findParasite, or findHost) that had citations = TRUE
-#'
+#' @param original Boolean. Should the output include the references that are 
+#'		not original sources (e.g. review or systematic study)? default is FALSE
 #' @return list of lists containing primary literature citation per interaction as provided by LMNH
 #'
 #' @author Anna Willoughby
 
 # input needs to be data.frame with Reference urls in column 4 and Citation count in column 5
-findCitations <- function(interactions = NULL){
-  ref_list <- list() # prepare a container
-  for(i in 1:nrow(interactions)){
-    url <- interactions[i,4] # first reference link, will need to loop for all
-    webpage <- read_html(url) #pull text from webpage
-    cdtble <- html_node(webpage, "table") # extract reference table from webpage
-    cdf <- html_table(cdtble, fill = TRUE) # convert to dataframe
-    names(cdf) <- c("ref_id", "variable", "value") # name columns
-    cdf <- cdf[which(is.na(str_extract(cdf$variable, "\\d")) == TRUE),]
-    cdf <- cdf[,c(1,2,3)] # only keep columns of interest
-    cdf$ref_id <- as.integer(substr(cdf$ref_id, 1, nchar(cdf$ref_id)-1)) # extract out count number
-    cdf <- fill(cdf, ref_id) # fill in ref_id
-    cdf <- dcast(cdf, ref_id ~ variable, fill = "", drop = FALSE, value.var = "value") # transform to see all metadata
-    tmp <- cdf$`Reference:` # select just the references
-    tmp <- gsub("\r|\n", "", tmp) # clean up references
-    name <- paste('interaction_',i,'_references', sep='')
-    ref_list[[name]] <- tmp # add to final dataframes
+findCitations <- function(interactions = NULL, original = FALSE){
+  if(!original) {
+    ref_list <- list() # prepare a container
+    for(i in 1:nrow(interactions)){
+      url <- interactions[i,4] # first reference link, will need to loop for all
+      webpage <- read_html(url) #pull text from webpage
+      cdtble <- html_node(webpage, "table") # extract reference table from webpage
+      cdf <- html_table(cdtble, fill = TRUE) # convert to dataframe
+      names(cdf) <- c("ref_id", "variable", "value") # name columns
+      cdf <- cdf[which(is.na(str_extract(cdf$variable, "\\d")) == TRUE),]
+      cdf <- cdf[,c(1,2,3)] # only keep columns of interest
+      cdf$ref_id <- as.integer(substr(cdf$ref_id, 1, nchar(cdf$ref_id)-1)) # extract out count number
+      cdf$variable <- substr(cdf$variable, 1, nchar(cdf$variable)-1) # remove colon
+      cdf <- fill(cdf, ref_id) # fill in ref_id
+      cdf <- dcast(cdf, ref_id ~ variable, fill = "", drop = FALSE, value.var = "value") # transform to see all metadata
+      tmp <- cdf$Reference # select just the references
+      tmp <- gsub("\r|\n", "", tmp) # clean up references
+      name <- paste('interaction_',i,'_references', sep='')
+      ref_list[[name]] <- tmp # add to final dataframes
     }
-  return(ref_list)
-}
-
+    return(ref_list) }
+  
+  if(original){
+    ref_list <- list() # prepare a container
+    for(i in 1:nrow(interactions)){
+      url <- interactions[i,4] # first reference link, will need to loop for all
+      webpage <- read_html(url) #pull text from webpage
+      cdtble <- html_node(webpage, "table") # extract reference table from webpage
+      cdf <- html_table(cdtble, fill = TRUE) # convert to dataframe
+      names(cdf) <- c("ref_id", "variable", "value") # name columns
+      cdf <- cdf[which(is.na(str_extract(cdf$variable, "\\d")) == TRUE),]
+      cdf <- cdf[,c(1,2,3)] # only keep columns of interest
+      cdf$ref_id <- as.integer(substr(cdf$ref_id, 1, nchar(cdf$ref_id)-1)) # extract out count number
+      cdf$variable <- substr(cdf$variable, 1, nchar(cdf$variable)-1) # remove colon
+      cdf <- fill(cdf, ref_id) # fill in ref_id
+      cdf <- dcast(cdf, ref_id ~ variable, fill = "", drop = FALSE, value.var = "value") # transform to see all metadata
+      cdf <- mutate(cdf, Comments = ifelse(is.null(cdf$Comments) == TRUE,"", cdf$Comments))
+      cdf <- cdf[which(cdf$Comments != "not original"),]
+      cdf <- cdf[which(cdf$Comments != "Not original"),]
+      cdf <- cdf[which(cdf$Comments != "not original, epidemiology reviewed"),]
+      cdf <- cdf[which(cdf$Comments != "systematics"),]
+      cdf <- cdf[which(cdf$Comments != "larvae, not original"),]
+      cdf <- cdf[which(cdf$Comments != "Not original, Mexican National Collections"),]
+      tmp <- cdf$Reference # select just the references
+      tmp <- gsub("\r|\n", "", tmp) # clean up references
+      name <- paste('interaction_',i,'_references', sep='')
+      ref_list[[name]] <- tmp # add to final dataframes
+    }
+    return(ref_list) }
+}  
 # currently this limits to 30 references per interaction
