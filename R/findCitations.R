84f67c7ff2bb10783ba8c60c113f33fb176bb76b
arw36 <arwill19@gmail.com>
2020-01-06 12:42:30 -0500

    change output to list, clean r\n\

--- R/findCitations.R
+++ R/findCitations.R
@@ -10,25 +10,30 @@
 #' @author Anna Willoughby
 
 library(xml2)
-library(purrr)
 library(rvest)
+library(tidyr)
+library(reshape2)
 
-# input needs to be data.frame with Reference urls in column 4
+# input needs to be data.frame with Reference urls in column 4 and Citation count in column 5
 findCitations <- function(interactions = NULL){
-  ref_df  <- NULL;
+  ref_list <- list() # prepare a container
   for(i in 1:nrow(interactions)){
     url <- interactions[i,4] # first reference link, will need to loop for all
     webpage <- read_html(url) #pull text from webpage
     cdtble <- html_node(webpage, "table") # extract reference table from webpage
     cdf <- html_table(cdtble, fill = TRUE) # convert to dataframe
-    cdf <- cdf[,c(2,3)] # only keep columns of interest
-    names(cdf) <- c("variable", "citations") # name columns
-    cdf <- filter(cdf, variable == "Reference:") # filter to just the reference
-    cdf$variable <- NULL # remove column
-    ref_df <- rbind(ref_df, cdf) # add to final dataframes
+    names(cdf) <- c("ref_id", "variable", "value") # name columns
+    cdf <- cdf[which(is.na(str_extract(cdf$variable, "\\d")) == TRUE),]
+    cdf <- cdf[,c(1,2,3)] # only keep columns of interest
+    cdf$ref_id <- str_extract(cdf$ref_id, "\\d")
+    cdf <- fill(cdf, ref_id) # fill in ref_id, relies on tidyr
+    cdf <- dcast(cdf, ref_id ~ variable, fill = "", drop = FALSE, value.var = "value")
+    tmp <- cdf$`Reference:`
+    tmp <- gsub("\r|\n", "", tmp) # clean up references
+    name <- paste('interaction_',i,'_references', sep='')
+    ref_list[[name]] <- tmp # add to final dataframes
     }
-  ref_df %<>% unique() # remove duplicates
-  return(ref_df)
+  return(ref_list)
 }
 
 # currently this limits to 30 references per interaction
