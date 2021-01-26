77528c9f52cf6b1fab2c4f35522ae83ba2905670
taddallas <tdallas@uga.edu>
2021-01-26 14:47:07 -0600

    taxize integration to replace col

--- tests/testthat/test-findHost.R
+++ tests/testthat/test-findHost.R
@@ -4,7 +4,7 @@ test_that("findHost finds the host", {
   skip_on_cran()
 	mousey <- findHost(genus="Peromyscus", species="leucopus", citation=TRUE)
 	expect_is(mousey, "data.frame")
-	expect_more_than(nrow(mousey), 1)
+	expect_gt(nrow(mousey), 1)
 
 	nothing <- findHost(genus = "Nothing")
 	expect_equal(nrow(nothing),0)
